3004826d7db8aa0e93ae3b9d6caf481cb1381c62
cle1t0 <78626587+cle1t0@users.noreply.github.com>
2021-08-02 17:36:46 -0300

    Update and rename tarefa2.py to silmple-classificator.py

--- silmple-classificator.py
+++ silmple-classificator.py
@@ -1,8 +1,7 @@
-#Tarefa2 -  Thiago Nishimura de Sousa
-#O arquivo original txt foi editado, nomeando-se as colunas para melhor identificação das classes
-#Para esse exercícios foi utilizado "kernel tricks" para que a SVM pudesse trabalhar com dados sem um fronteira linear de decisão
+#The original dataset was edited, adding the first rows as label in order to make the feature/output division easier
+#Kernel tricks were used because of the non-linear dicision boundary
 
-#Bibliotecas utilizadas
+#Libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,28 +10,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 
-#Lendo o arquivo
+#Reading the archive
 data =  pd.read_csv('vetsRots.txt',sep=' ',header=0) 
 #print(data.head)
 
-#Processando os dados, para poder utilizar a coluna "class" como rótulo
+#Setting the 'class' column as the output
 x = data.drop('class',axis=1)
 y = data['class']
 
-#Pedindo ao usuario para colocar a porcentagem de dados para teste
+#Here I ask the user to define a percentage for the train-test split
 val = input("Insira a porcentagem de treino : ") 
 val =  int(val)
 print('O treino será de',val,'%')
 valtreino  = (val/100)
 
-#Separando os dados entre treino e teste
+#Trani-test split
 x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = valtreino)
 
-#Treinando a SVM com o modelo de Kernel Gaussiano
+#Training the model using the Gaussian Kernel method
 maquina = SVC(kernel='rbf')
 maquina.fit(x_treino, y_treino)
 
-#Previsão e Acurácia
+#Predictions and accuracy response
 y_prever = maquina.predict(x_teste)
 print(classification_report(y_teste, y_prever))
 
